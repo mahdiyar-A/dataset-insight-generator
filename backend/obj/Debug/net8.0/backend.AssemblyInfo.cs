7482ca305d223ed7c024642b4830142e1ab36ab4
Mahdiyar Ashrafioun <mahdiyar.ashrafioun@ucalgary.ca>
2026-01-27 18:16:41 -0700

    pushing everything

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4935fa073df52a9f6d0614117d3b5efa577212d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e931158d5034f1b9f88a408a90146dbf07b8d5f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
